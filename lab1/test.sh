3c5f2cdce4d27b5cf1b14d6303987d1fbdd93ab4
totoro <953834564@qq.com>
2021-05-24 22:46:12 +0800

    search ok

--- lab1/test.sh
+++ lab1/test.sh
@@ -4,14 +4,14 @@ if [[ $(grep 'import' myImpl.py) != 'import util' ]]; then
 fi
 
 cd search
-python autograder.py -q q1
-python pacman.py -l mediumMaze -p SearchAgent --frameTime 0
-python autograder.py -q q2
-python pacman.py -l mediumMaze -p SearchAgent -a fn=bfs --frameTime 0
-python autograder.py -q q3
-python pacman.py -l mediumMaze -p SearchAgent -a fn=astar,heuristic=manhattanHeuristic --frameTime 0
-cd ../multiagent
-python autograder.py -q q2 --no-graphics
-python autograder.py -q q3 --no-graphics
-python pacman.py -p AlphaBetaAgent -l mediumClassic --frameTime 0
+python3 autograder.py -q q1
+python3 pacman.py -l mediumMaze -p SearchAgent --frameTime 0
+python3 autograder.py -q q2
+python3 pacman.py -l mediumMaze -p SearchAgent -a fn=bfs --frameTime 0
+python3 autograder.py -q q3
+python3 pacman.py -l mediumMaze -p SearchAgent -a fn=astar,heuristic=manhattanHeuristic --frameTime 0
+# cd ../multiagent
+# python3 autograder.py -q q2 --no-graphics
+# python3 autograder.py -q q3 --no-graphics
+# python3 pacman.py -p AlphaBetaAgent -l mediumClassic --frameTime 0
 
